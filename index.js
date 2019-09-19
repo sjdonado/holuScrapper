b2e2c030ee2586fdff013e1d98ced5c0e124b12e
Jaime Ramos <jrossetes@uninorte.edu.co>
2019-09-18 23:32:49 -0500

    buenas practicas

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const puppeteer = require('puppeteer');
 const express = require("express")
 const app = express()
 const bcrypt = require('bcrypt')
 const mongohandler = require('./Mongolib/index')
+const scrapper= require('./scrappers/uninorte')
 //const config=require('dotenv').config()
 app.listen(3000);
 console.log('server started on port 3000');
@@ -14,17 +14,18 @@ app.get('/completeRegister/:userApp/:passApp/:telefono/:universidad/:userU/:pass
     let uni=req.params.universidad
     let uniUser=req.params.userU
     let uniPass=req.params.passU
-    await run(uniUser, uniPass);
+    const vector =await scrapper(uniUser, uniPass);
     res.send("DONE!")
     const hashedpassword = await bcrypt.hash(pass, 10)
-    let c = await mongohandler.crearNuevoUsuarioConHorario(user,hashedpassword,number,uni,lunes2,martes2,miercoles2, jueves2, viernes2, sabado2, domingo2)
+    let c = await mongohandler.crearNuevoUsuarioConHorario(user,hashedpassword,number,uni,vector[0],vector[1],vector[2],vector[3], vector[4], vector[5], vector[6])
 })
-app.get('/:user/:pass', async function (req, res) {//para cuando una persona que al registrarse no quiso importar su horario y apenas ahora lo va a hacer
-    let user = req.params.user
-    let pass = req.params.pass
-    await run(user, pass);
-    res.send("DONE!")
-    let c = await mongohandler.actualizar(user,lunes2,martes2,miercoles2, jueves2, viernes2, sabado2, domingo2)
+app.get('/:Appuser/:universityUser/:universityPass', async function (req, res) {//para cuando una persona que al registrarse no quiso importar su horario y apenas ahora lo va a hacer
+    let user = req.params.Appuser
+    let userU=req.params.universityUser
+    let passU = req.params.universityPass
+    const vector=await scrapper(userU, passU)
+    res.json("DONE!")
+    let c = await mongohandler.actualizar(user,vector[0],vector[1],vector[2],vector[3], vector[4], vector[5], vector[6])
 })
 app.get('/directRegister/:user/:pass/:telefono/:universidad', async function (req, res) {//registro de un nuevo usuario de manera directa, es decir, sin usar instagram fb etc y sin importar su horario con nuestro scrapper
     console.log("se conectaron")
@@ -53,150 +54,8 @@ app.get('/login/:user/:pass', async function (req, res) {
         console.log(error);
     });
 })
-
-let horario = [], lunes2 = [], martes2 = [], miercoles2 = [], jueves2 = [], viernes2 = [], sabado2 = [], domingo2 = []
-
-async function run(user, pass) {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-    try {
-        const USERNAME_SELECTOR = '#UserID'
-        const PASSWORD_SELECTOR = '#PIN > input[type=password]'
-        const INFOACADEMICA_SELECTOR = 'body > div.pagebodydiv > table.menuplaintable > tbody > tr:nth-child(1) > td:nth-child(2) > a'
-        const MATRICULA_SELECTOR = 'body > div.pagebodydiv > table.menuplaintable > tbody > tr:nth-child(2) > td:nth-child(2) > a'
-        const HORARIO_SELECTOR = 'body > div.pagebodydiv > table.menuplaintable > tbody > tr:nth-child(4) > td:nth-child(2) > a'
-        const SELECT_SELECTOR = '#term_id'
-        const BOTON_SELECTOR = 'body > div.pagebodydiv > form > input[type=submit]'
-        const page = await browser.newPage();
-        await page.goto('https://pomelo.uninorte.edu.co/pls/prod/twbkwbis.P_WWWLogin');
-        await page.click(USERNAME_SELECTOR);
-        await page.keyboard.type(user)
-        await page.click(PASSWORD_SELECTOR);
-        await page.keyboard.type(pass)
-        await page.click('body > div.pagebodydiv > form > p > input[type=submit]')
-        await page.waitForNavigation()
-        await page.click(INFOACADEMICA_SELECTOR)
-        await page.click(MATRICULA_SELECTOR)
-        await page.click(HORARIO_SELECTOR)
-        await page.select(SELECT_SELECTOR, '201930')
-        await page.click(BOTON_SELECTOR)
-        await page.waitForSelector('body > div.pagebodydiv');
-        await page.waitFor(10000);
-        const data = await page.evaluate(() =>
-            Array.from(document.querySelectorAll('body div.pagebodydiv table.datadisplaytable[summary="Esta tabla lista los horarios de reunión calendarizados y los instructores asignados para esta clase.."] tbody tr td.dddefault')).map(r => r.innerText)
-        )
-
-        matrix2 = []
-        i = 0
-        let h1, h2, dd, ii = [], aux
-        for (let d in data) {
-            let c = new clase()
-            if (i == 4) {
-                ii = []
-                aux = data[d]
-                ii.push(data[d])
-            } else {
-                if (i == 2) {
-                    dd = data[d]
-                } else {
-                    if (i == 1) {
-                        let nuevo = data[d].split("-")
-                        h1 = nuevo[0].trim()
-                        h2 = nuevo[1].trim()
-                    }
-                }
-            }
-            i++
-            if (i == 7) {
-                if (verificar(matrix2, h1, h2, dd, aux)) {
-                    let c = new clase(h1, h2, dd, ii)
-                    matrix2.push(c)
-                }
-                i = 0
-            }
-        }
-
-        for (let i in matrix2) {
-            let o = new clases(matrix2[i].horainicial, matrix2[i].horafinal, matrix2[i].intervalos)
-            if (matrix2[i].dia === "V") {
-                viernes2.push(o)
-            } else {
-                if (matrix2[i].dia === "J") {
-                    jueves2.push(o)
-                } else {
-                    if (matrix2[i].dia === "I") {
-                        miercoles2.push(o)
-                    } else {
-                        if (matrix2[i].dia === "M") {
-                            martes2.push(o)
-                        } else {
-                            if (matrix2[i].dia === "L") {
-                                lunes2.push(o)
-                            } else {
-                                if (matrix2[i].dia === "D") {
-                                    domingo2.push(o)
-                                } else {
-                                    if (matrix2[i].dia === "S") {
-                                        sabado2.push(o)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        horario.push(lunes2)
-        horario.push(martes2)
-        horario.push(miercoles2)
-        horario.push(jueves2)
-        horario.push(viernes2)
-        horario.push(sabado2)
-        horario.push(domingo2)
-        let pages = await browser.pages();
-        pages.forEach(async (page) => await page.close());
-    } catch (e) {
-        console.log(e)
-    } finally {
-    }
-}
-
-function verificar(m, hi, hf, ddd, a) {
-    if (m.length == 0) {
-        return true
-    } else {
-        for (let i in m) {
-            if (m[i].horainicial == hi && m[i].horafinal == hf && matrix2[i].dia == ddd) {
-                m[i].intervalos.push(a)
-                return false
-            }
-        }
-    }
-    return true
-}
-
-class clase {
-
-    constructor(horai, horaf, dia, intervalos) {
-        this.horainicial = horai
-        this.horafinal = horaf
-        this.dia = dia
-        this.intervalos = intervalos
-    }
-
-}
-class clases {
-
-    constructor(horai, horaf, intervalos) {
-        this.horainicial = horai
-        this.horafinal = horaf
-        this.intervalos = intervalos
-    }
-
-}
 module.exports = app;
-//debo orientar todo esto a objetos
-
-    //SIMON
+//SIMON
 /*
 const browser = await puppeteer.launch({ headless: false, timeout: 0 });
 const page = await browser.newPage();
